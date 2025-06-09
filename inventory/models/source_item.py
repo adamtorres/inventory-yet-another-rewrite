f549e90afc4205ae8a62b7200e48ca1b916b005a
Adam Torres <adamprogrammer@gmail.com>
2025-06-09 12:50:15 -0600

    Added cryptic, expanded, and common names to SourceItem. Added the item and extra number/codes to SourceItem.

--- inventory/models/source_item.py
+++ inventory/models/source_item.py
@@ -4,6 +4,25 @@ from django.db import models
 class SourceItem(models.Model):
     source = models.ForeignKey("inventory.Source", on_delete=models.DO_NOTHING)
     discontinued = models.BooleanField(default=False)
+
+    # Note: Some sources reuse codes.  Not frequently, but it has happened.
+    item_number = models.CharField(
+        max_length=255, help_text="The main number/code used to identify this product at this source.", null=False,
+        blank=True, default="")
+    extra_number = models.CharField(
+        max_length=255, help_text="Some sources have a second identifying number/code.", null=False, blank=True,
+        default="")
+
+    cryptic_name = models.CharField(
+        max_length=1024, help_text="The horribly abbreviated and truncated name as it appears on the invoice.")
+    expanded_name = models.CharField(
+        max_length=1024,
+        help_text="The horribly abbreviated and truncated name but with as many of the abbreviations and truncations "
+                  "fixed.", null=False, blank=True, default="")
+    common_name = models.CharField(
+        max_length=1024, help_text="A more friendly version of the name.  Would this just be the Item's name?",
+        null=False, blank=True, default="")
+
     item = models.ForeignKey("inventory.item", on_delete=models.DO_NOTHING)
     brand = models.CharField(max_length=255)
     # pack/size/unit/count options
