0266fc8849081a8906bafb101c1f5cc284ac96a5
Adam Torres <adamprogrammer@gmail.com>
2025-07-06 12:17:07 -0600

    Added models to admin site. Fixed Category plural on admin site.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import forms as inv_forms, models as inv_models
+
+
+admin.AdminSite.site_header = "Customized Admin Site Header From Inventory"
+
+admin.site.register(inv_models.Category)
+admin.site.register(inv_models.Item)
+admin.site.register(inv_models.Order)
+admin.site.register(inv_models.Source)
+admin.site.register(inv_models.SourceItem)
+admin.site.register(inv_models.UnitSize)
