48ce2f6be3e2df1bfc86069cd0c99d8507c77d82
Adam Torres <adamprogrammer@gmail.com>
2025-08-24 11:36:54 -0600

    Converted ingredient pages to only be (mostly) accessible from recipe pages.  Similar to OrderLineItem.

--- meal_planning/urls.py
+++ meal_planning/urls.py
@@ -9,16 +9,25 @@ app_name = "meal_planning"
 urlpatterns = [
     # urls.path("api/category", i_views.APICategoryView.as_view(), name="api_category"),
     urls.path("ingredients/", mp_views.IngredientListView.as_view(), name="ingredient_list"),
-    urls.path("ingredient/<int:pk>", mp_views.IngredientDetailView.as_view(), name="ingredient_detail"),
-    urls.path("ingredient/<int:pk>/delete", mp_views.IngredientDeleteView.as_view(), name="ingredient_delete"),
-    urls.path("ingredient/<int:pk>/edit", mp_views.IngredientUpdateView.as_view(), name="ingredient_update"),
-    urls.path("ingredient/new", mp_views.IngredientCreateView.as_view(), name="ingredient_create"),
 
     urls.path("recipes/", mp_views.RecipeListView.as_view(), name="recipe_list"),
     urls.path("recipe/<int:pk>", mp_views.RecipeDetailView.as_view(), name="recipe_detail"),
     urls.path("recipe/<int:pk>/delete", mp_views.RecipeDeleteView.as_view(), name="recipe_delete"),
     urls.path("recipe/<int:pk>/edit", mp_views.RecipeUpdateView.as_view(), name="recipe_update"),
     urls.path("recipe/new", mp_views.RecipeCreateView.as_view(), name="recipe_create"),
+
+    urls.path(
+        "recipe/<int:recipe_pk>/ingredient/<int:pk>", mp_views.IngredientDetailView.as_view(),
+        name="recipe_ingredient_detail"),
+    urls.path(
+        "recipe/<int:recipe_pk>/ingredient/<int:pk>/delete", mp_views.IngredientDeleteView.as_view(),
+        name="recipe_ingredient_delete"),
+    urls.path(
+        "recipe/<int:recipe_pk>/ingredient/<int:pk>/edit", mp_views.IngredientUpdateView.as_view(),
+        name="recipe_ingredient_update"),
+    urls.path(
+        "recipe/<int:recipe_pk>/ingredient/new", mp_views.IngredientCreateView.as_view(),
+        name="recipe_ingredient_create"),
 
     urls.path("", mp_views.IngredientHomepageView.as_view(), name="homepage"),
 ]
--- meal_planning/views/ingredient.py
+++ meal_planning/views/ingredient.py
@@ -14,18 +14,41 @@ class IngredientCreateView(generic.CreateView):
     model = mp_models.Ingredient
     fields = ["recipe", "name", "category", "unit_size", "unit_amount"]
 
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['recipe'] = self.kwargs['recipe_pk']
+        return initial
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["recipe_pk"] = self.kwargs['recipe_pk']
+        return context
+
     def get_success_url(self):
-        return urls.reverse("meal_planning:ingredient_detail", args=(self.object.id,))
+        if "save_and_add_another" in self.request.POST:
+            return urls.reverse("meal_planning:recipe_ingredient_create", args=(self.object.recipe.pk,))
+        return urls.reverse("meal_planning:recipe_detail", args=(self.object.recipe.pk,))
 
 
 class IngredientDeleteView(generic.DeleteView):
     model = mp_models.Ingredient
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["recipe_pk"] = self.kwargs['recipe_pk']
+        return context
+
     def get_success_url(self):
-        return urls.reverse("meal_planning:ingredient_list")
+        return urls.reverse("meal_planning:recipe_detail", args=(self.object.recipe.pk,))
+
 
 class IngredientDetailView(generic.DetailView):
     queryset = mp_models.Ingredient.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["recipe_pk"] = self.kwargs['recipe_pk']
+        return context
 
 
 class IngredientListView(generic.ListView):
@@ -37,5 +60,10 @@ class IngredientUpdateView(generic.UpdateView):
     model = mp_models.Ingredient
     fields = ["recipe", "name", "category", "unit_size", "unit_amount"]
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["recipe_pk"] = self.kwargs['recipe_pk']
+        return context
+
     def get_success_url(self):
-        return urls.reverse("meal_planning:ingredient_detail", args=(self.object.id,))
+        return urls.reverse("meal_planning:recipe_ingredient_detail", args=(self.object.recipe.id, self.object.id,))
