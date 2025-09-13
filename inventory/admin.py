85bdfc3c3be706063c9865aa067306845838a43f
Adam Torres <adamprogrammer@gmail.com>
2025-09-13 17:32:33 -0600

    Fixed copy pasta error in passing the item to the item_list_item.html template. Changed item list from ul to table. Expanded inventory.Item's admin page. Added link from category detail to item list defaulting to filter by the selected category.

--- inventory/admin.py
+++ inventory/admin.py
@@ -5,9 +5,22 @@ from . import forms as inv_forms, models as inv_models
 
 admin.AdminSite.site_header = "Customized Admin Site Header From Inventory"
 
+
+@admin.register(inv_models.Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'category',
+        'description',
+    )
+    search_fields = ('name', 'description')
+    list_filter = ('category__name',)
+
+
 admin.site.register(inv_models.Category)
 admin.site.register(inv_models.Conversion)
-admin.site.register(inv_models.Item)
+# admin.site.register(inv_models.Item)
 admin.site.register(inv_models.Order)
 admin.site.register(inv_models.OrderLineItem)
 admin.site.register(inv_models.Setting)
