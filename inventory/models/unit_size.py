23fcc2b96d4cc64bf8daa786a2225c454ca0d867
Adam Torres <adamprogrammer@gmail.com>
2025-09-22 22:29:48 -0600

    Starting to convert SourceItem to a different unit_size method. Migrations added to copy existing data to new fields and remove previous fields.  Reverse migration should work out. Modified str method to work.

--- inventory/models/unit_size.py
+++ inventory/models/unit_size.py
@@ -8,9 +8,14 @@ class UnitSize(models.Model):
     """
     # description = "#10 can", "15 floz can", "16oz bag", "50lb bag", "5 gallon bucket"
     unit = models.CharField(max_length=1024, help_text="oz, floz, lb, pint, gallon")
+    # TODO: split `unit` into `name` and `abbr`?
 
     class Meta:
         ordering = ["unit"]
 
     def __str__(self):
         return "pk" if self.unit == "subunit" else self.unit
+
+    @property
+    def splittable(self):
+        return self.unit == "count"
