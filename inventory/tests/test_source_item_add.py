4212d4fb6e9bb9774ba37f81db9d857423b6e0e6
Adam Torres <adamprogrammer@gmail.com>
2025-09-25 20:10:00 -0600

    Revert "Should've started a branch for this change as there are other things that need done before this will be finished."
    This reverts commit 850d18d0d491acd65d37432b4d733c403c7669e3.

--- inventory/tests/test_source_item_add.py
+++ inventory/tests/test_source_item_add.py
@@ -15,8 +15,8 @@ class TestSourceItemAdd(TestCase):
         # defaults: active=True, allow_split_pack=True
         si = inv_models.SourceItem.objects.create(
             source=self.sysco, cryptic_name="the cryptic name", expanded_name="the expanded name",
-            common_name="the common name", item=self.butter, brand="the brand", delivered_package_unit_size=self.pound,
-            delivered_package_amount=1, source_category="the source category"
+            common_name="the common name", item=self.butter, brand="the brand", unit_size=self.pound,
+            unit_amount=1, source_category="the source category"
         )
         self.assertTrue(si.active)
         self.assertTrue(si.allow_split_pack)
