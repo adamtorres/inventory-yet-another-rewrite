8d86637c77740df66f9d9e9b3bf333cd8192bfe2
Adam Torres <adamprogrammer@gmail.com>
2025-10-11 23:09:48 -0600

    Added function descriptions. Renamed a variable or two for clarity. Removed result_style_ids and active_result_styles as they are no longer used.

--- static/js/search.js
+++ static/js/search.js
@@ -1,11 +1,10 @@
 let srch_keypress_timer;
 var srch_url = "url for the json API";
 
-// Matches the keys in result_style_ids.  Only those in this list will be populated with search results.
-var active_result_styles = ["div"];
-
+// id of the form to look for INPUT search elements.
 var srch_form_id = "search_form";
 
+// event name for after successfully populating results even if there are none.
 const srch_post_populate_results_name = 'srch-post-popultate-results';
 
 // event triggered after the results are added to the page.
@@ -14,26 +13,8 @@ const srch_post_popultate_results = new CustomEvent(srch_post_populate_results_n
 // custom click event attached to each new search result.
 const srch_result_onclick_name = "srch-result-onclick";
 
+// signal to remove events added to search results.
 let srch_abort_controller = new AbortController();
-
-var result_style_ids = {
-    // The value of the key is arbitrary.  It just represents one of the output styles.
-    "div": {
-        //A containing element, usually a div, which wholly contains everything for this result type.
-        "container": "results_in_div",
-
-        // An element which will be completely emptied and filled as the searches happen.
-        "results": "search_results_div",
-
-        // A full template of how an individual result should appear.  Elements with "data-field" attributes will have
-        // their contents replaced as needed.  The value of "data-field" should match a field in the response JSON.
-        "template": "search_result_template_div",
-
-        // A full template with content which represents no search results.  This will be copied into
-        // "search_results_div" as needed.
-        "no_results_template": "search_no_result_template_div",
-    },
-}
 
 // an array of element ids to use as templates for new items and the destination for those new items.
 var result_ids = [
@@ -97,6 +78,10 @@ function srch_add_result(item_to_add) {
 }
 
 function srch_convert_values_to_query_string(values_to_send) {
+    /*
+    * :param values_to_send: dict with key "search_terms" to encode.  Optionally includes an "echo" key which is not
+    * modified by the API and serves as a way to identify this call.
+    * */
     const params = new URLSearchParams();
     for (const pair of values_to_send.search_terms) {
         params.append(pair.key, pair.value);
@@ -108,6 +93,9 @@ function srch_convert_values_to_query_string(values_to_send) {
 }
 
 function srch_filter_keydown(e) {
+    /*
+    * Filters the keydown event to a specific set of keys.
+    * */
     if (srch_key_is_visible(e)) {
         srch_start_timer(e.target);
     } else if (!srch_key_is_not_visible(e)) {
@@ -115,15 +103,16 @@ function srch_filter_keydown(e) {
     }
 }
 
-function srch_get_href_args(e) {
+function srch_get_href_args(element) {
     /*
     Returns the data attributes of element e which are named "data-href-arg-*".
     <a ... data-href-arg-order="x" data-href-arg-line-item="y">
     returns {"order": "x", "line-item": "y"}
+    :param e: element containing the "data-href-*" attributes.
     */
     const attributes = {};
-    for (let i = 0; i < e.attributes.length; i++) {
-        const attribute = e.attributes[i];
+    for (let i = 0; i < element.attributes.length; i++) {
+        const attribute = element.attributes[i];
         if (attribute.name.startsWith('data-href-arg-')) {
             attributes[attribute.name.slice(14)] = attribute.value;
         }
@@ -132,12 +121,17 @@ function srch_get_href_args(e) {
 }
 
 function srch_get_no_result_element_template(_result_style) {
-    // The root of the template used for indicating no results.
+    /*
+    * Returns the element to use as a template when the search returned no results.
+    * :param _result_style: dict of a single result style from result_ids.
+    * */
     return document.getElementById(_result_style["no_results_template_id"]);
 }
 
 function srch_get_result_element(_result_style) {
-    // The root of the container element for search results.
+    /*
+    * Returns the element that will be emptied and filled with search results.
+    * */
     // TODO: This needs to handle form-named fields.  "{{ widget.attrs.id }}-search-results-div"
     return document.getElementById(_result_style["result_element_id"]);
 }
@@ -161,12 +155,18 @@ function srch_get_result_element_template(_result_style) {
 }
 
 function srch_get_search_elements() {
+    /*
+    * Returns all elements to be used as search inputs.
+    * */
     // console.log(`srch_get_search_elements for srch_form_id = "${srch_form_id}"`);
     const form = document.getElementById(srch_form_id);
     return form.querySelectorAll("[data-search-field]");
 }
 
 function srch_get_values_to_send() {
+    /*
+    * Builds a dict containing the values to search for.  If nothing to use, an `empty` flag is `true`.
+    * */
     let values_to_send = {
         empty: true,
         "search_terms": []
@@ -181,10 +181,16 @@ function srch_get_values_to_send() {
 }
 
 function srch_input_focusout() {
+    /*
+    * When an input loses focus, clear the timer so the search does not happen.
+    * */
     window.clearTimeout(srch_keypress_timer);
 }
 
 function srch_key_is_not_visible(e) {
+    /*
+    * Is the key specified by `e` a non-printing key?
+    * */
     let ignore = [
         'ShiftLeft', 'MetaLeft', 'AltLeft', 'ControlLeft',
         'ShiftRight', 'MetaRight', 'AltRight', 'ControlRight',
@@ -196,6 +202,9 @@ function srch_key_is_not_visible(e) {
 }
 
 function srch_key_is_visible(e) {
+    /*
+    * Is the key specified by `e` a visible key we care about?
+    * */
     let donot_ignore = [
         "Backspace", "Space", "Backquote", "Equal", "Minus", "Backslash", "BracketRight", "BracketLeft", "Quote",
         "Semicolon", "Slash", "Period", "Comma",
@@ -221,6 +230,10 @@ function srch_no_results() {
 }
 
 function srch_populate_results(data_packet) {
+    /*
+    * Given a data packet returned from the API, clear existing results and add in the new ones.
+    * Raises a custom event to allow the page to react when searches complete.
+    * */
     if (data_packet.data.length === 0) {
         srch_no_results();
         return;
@@ -255,6 +268,9 @@ function srch_remove_all_results() {
 }
 
 function srch_setup_events() {
+    /*
+    * Adds the event listeners for keys, focus, and click events on the inputs.
+    * */
     for (const element of srch_get_search_elements()) {
         if ((element.tagName === "INPUT") && (element.type === "text")) {
             // keydown and focusout events
@@ -274,6 +290,10 @@ function srch_setup_events() {
 }
 
 function srch_start_timer(e) {
+    /*
+    * Clears any existing timers and starts a new one.  This is used so typing at a "normal" speed will not result in
+    * multiple API calls.  Only when there is a more significant pause will an API call happen.
+    * */
     window.clearTimeout(srch_keypress_timer);
     srch_keypress_timer = setTimeout(() => {
         srch_timer_elapsed_func(e);
@@ -281,13 +301,17 @@ function srch_start_timer(e) {
 }
 
 function srch_timer_elapsed_func(caller_obj) {
+    /*
+    * This is what calls the API specified by `srch_url`.
+    * */
     let values_to_send = srch_get_values_to_send();
     if (values_to_send.empty) {
+        // nothing to send so don't bother trying.
         srch_no_results();
         return;
     }
     if (caller_obj != null) {
-        console.log(caller_obj);
+        // In limited cases, the caller_obj is not specified.  Cannot call hasAttributes on null.
         if (caller_obj.hasAttributes("id")) {
             values_to_send["echo"] = {"id": caller_obj.id};
         }
@@ -306,6 +330,10 @@ function srch_timer_elapsed_func(caller_obj) {
 // Using (function() {...})(); doesn't wait for the rest of the page to load.  Setting srch_form_id in the page isn't
 // applied yet.
 ready(() => {
+    /*
+    * The page-load function.  The `ready()` function from `not_jquery.js` waits until the page is completely loaded so
+    * the page's overriding of the variables at the top are applied.
+    * */
     srch_setup_events();
     srch_no_results();
 });
