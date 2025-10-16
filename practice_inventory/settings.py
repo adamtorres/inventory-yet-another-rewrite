efb5968b432915afe8be3206ce2ed6a097e965cf
Adam Torres <adamprogrammer@gmail.com>
2025-10-16 17:06:42 -0600

    Made SECRET_KEY and DEBUG environment variables.

--- practice_inventory/settings.py
+++ practice_inventory/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xvjz-m)i_)v&ebh(9xg^*58ugg48g9*e+yftosqfs@x=u6+zj$'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-xvjz-m)i_)v&ebh(9xg^*58ugg48g9*e+yftosqfs@x=u6+zj$")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['*']
 
