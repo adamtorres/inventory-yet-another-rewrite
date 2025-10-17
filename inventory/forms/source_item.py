9bb2a36480b62bf87ae75fe609d1ce285a34df0b
Adam Torres <adamprogrammer@gmail.com>
2025-10-16 22:19:18 -0600

    Converted Item's field on SourceItemCreateView to ModelPickerWidget.  Seems to work. Added 'wider_search' to APIItemView as that is what ModelPicker uses. Unrelated: Added source_pk to the popup for SourceItemCreateView so the popup defaults to selecting the Source of the current Order.

--- inventory/forms/source_item.py
+++ inventory/forms/source_item.py
@@ -4,6 +4,17 @@ from inventory import models as inv_models
 from inventory.forms import widgets as inv_widgets
 
 
+class SourceItemForm(forms.ModelForm):
+    item = forms.ModelChoiceField(queryset=inv_models.Item.objects.all(), widget=inv_widgets.ModelPickerWidget)
+    class Meta:
+        model = inv_models.SourceItem
+        fields = [
+            "source", "item_number", "extra_number", "cryptic_name", "expanded_name", "common_name",
+            "item", "brand", "source_category", "unit_size", "unit_amount", "unit_amount_text", "subunit_size",
+            "subunit_amount", "subunit_amount_text", "active", "quantity", "allow_split_pack"
+        ]
+
+
 class SourceItemTestDropDownForm(forms.Form):
     # template_name_ div, p, table, ul, label "django/forms/___.html"
     # template_name_table = "inventory/forms/source_item_test_drop_down_form_table.html"
